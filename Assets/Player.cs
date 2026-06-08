bfa647cd627b907e97b5f2b3958d7788cce71605
Hoon Jeong <hoon@hoon.local>
2026-06-08 15:42:38 -0700

    Implement Character Animation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,23 +2,34 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    [SerializeField] private float moveSpeed;
+
+    Rigidbody2D rb;
+    Animator anim;
+
+    float xInput;
 
     void Awake()
     {
-
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponentInChildren<Animator>();
     }
-
-    void Start()
-    {
-
-    }
-
-
 
     void Update()
     {
-        rb.linearVelocity = new Vector2(Input.GetAxisRaw("Horizontal"), rb.linearVelocityY);
-        print(Input.GetAxisRaw("Horizontal"));
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        HandleAnimations();
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        rb.linearVelocity = new Vector2(xInput * moveSpeed, rb.linearVelocityY);
+    }
+
+    void HandleAnimations()
+    {
+        anim.SetBool("isRunning", rb.linearVelocityX != 0);
     }
 }
