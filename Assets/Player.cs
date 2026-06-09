b7ed59402b8ee07dbd8c2a8a57b353dad1f3d9fc
Hoon Jeong <hoon@hoon.local>
2026-06-08 18:36:08 -0700

    Add Raycast on character

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,13 +1,20 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Collision info")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
 
     Rigidbody2D rb;
     Animator anim;
-
     float xInput;
+    bool isGrounded;
 
     void Awake()
     {
@@ -17,10 +24,22 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+        HandleCollision();
+        HandleInput();
+        HandleMovement();
+        HandleAnimations();
+    }
+
+    void HandleInput()
+    {
         xInput = Input.GetAxisRaw("Horizontal");
 
-        HandleAnimations();
-        HandleMovement();
+        if (Input.GetKey(KeyCode.Space) && isGrounded) { Jump(); }
+    }
+
+    void Jump()
+    {
+        rb.linearVelocity = new Vector2(rb.linearVelocityX, jumpForce);
     }
 
     void HandleMovement()
@@ -31,5 +50,15 @@ public class Player : MonoBehaviour
     void HandleAnimations()
     {
         anim.SetFloat("xVelocity", rb.linearVelocityX);
+    }
+
+    void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x, transform.position.y - groundCheckDistance));
     }
 }
